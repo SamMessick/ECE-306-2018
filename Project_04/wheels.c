5c4ec394055da26bb115080d88c91c7a76f6099a
shmessic <shmessic@ncsu.edu>
2018-02-08 16:01:54 -0500

    Update wheels.c

--- Project_04/wheels.c
+++ Project_04/wheels.c
@@ -45,22 +45,18 @@ void Wheels_OFF(void){
 // Forward *
 //**********
 void Left_Motor_ON_FORWARD(uint8_t speed){
-  switch(left_reverse_flag)                     // Do not attempt forward/backward driving at one time
+  if(left_reverse_flag)                         // If left motor is not set to move backward already
   {
-  case true: break;
-  case false:
-    TB0CCR4 = speed*DUTY_CYCLE_DIVISOR;         // Set speed as one of 255 settings
-    left_forward_flag = true;
+    TB0CCR4 = speed*DUTY_CYCLE_DIVISOR;         // Set left motor forward speed as one of 255 settings
+    left_forward_flag = true;                   // Prevent left motor from moving backward simultaneously
   }
 }
 
 void Right_Motor_ON_FORWARD(uint8_t speed){
-  switch(right_reverse_flag)                    // Do not attempt forward/backward driving at one time
-  {
-  case true: break;
-  case false: 
-    TB0CCR6 = speed*DUTY_CYCLE_DIVISOR;         // Set speed as one of 255 settings
-    right_forward_flag = true;
+  if(right_reverse_flag)                        // If right motor is not set to move backward already
+  { 
+    TB0CCR6 = speed*DUTY_CYCLE_DIVISOR;         // Set right motor forward speed as one of 255 settings
+    right_forward_flag = true;                  // Prevent right motor from moving backward simultaneously
   }
 }
 
@@ -73,22 +69,18 @@ void Drive_Straight_Forward(uint8_t speed){
 // Reverse *
 //**********
 void Left_Motor_ON_REVERSE(uint8_t speed){
-  switch(left_forward_flag)                     // Do not attempt forward/backward driving at one time
-  {
-  case true: break;
-  case false: 
-    TB0CCR3 = speed*DUTY_CYCLE_DIVISOR;         // Set speed as one of 255 settings
-    left_reverse_flag = true;
+  if(!left_forward_flag)                        // If left motor is not set to move forward already
+  { 
+    TB0CCR3 = speed*DUTY_CYCLE_DIVISOR;         // Set left motor reverse speed as one of 255 settings
+    left_reverse_flag = true;                   // Prevent left motor from moving forward simultaneously
   }
 }
 
 void Right_Motor_ON_REVERSE(char speed){
-  switch(right_forward_flag)                    // Do not attempt forward/backward driving at one time
+  if(!right_forward_flag)                       // If right motor is not set to move forward already
   {
-  case true: break;
-  case false: 
-    TB0CCR5 = speed*DUTY_CYCLE_DIVISOR;         // Set speed as one of 255 settings
-    right_reverse_flag = true;
+    TB0CCR5 = speed*DUTY_CYCLE_DIVISOR;         // Set right motor reverse speed as one of 255 settings
+    right_reverse_flag = true;                  // Prevent right motor from moving forward simultaneously
   }
 }
 
@@ -100,114 +92,121 @@ void Drive_Straight_REVERSE(uint8_t speed){
 //------------------------
 // Driving in shapes
 //------------------------
-
+//**********
+// Circle  *
+//**********
 void drive_in_circle(void){
-  static uint8_t edges_left_to_drive = NUM_TRIALS;
+  static uint8_t circles_left_to_drive = NUM_TRIALS;
   static uint8_t instruction_label;
-  switch(shape_routine_begin)
-  {
-  case true:
+  
+  if(shape_routine_begin)
     switch(instruction_label)
     {
-    case INSTRUCTION1: // wait two seconds before beginning
-      delay_time = TWO_SEC;                   // send delay time to global accessible by timer A1
-      TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
+        
+    case INSTRUCTION1: /* wait two seconds before beginning */
+      delay_time = TWO_SEC;                         // send delay time to global accessible by timer A1
+      //TA1CCTL2 |= CCIE;                           // enable timer A1.2 to count time
       instruction_label++; break;
-    case INSTRUCTION2: // drive in circle counterclockwise
+        
+        
+    case INSTRUCTION2: /* drive in circle counterclockwise */
       Left_Motor_ON_FORWARD(LEFT_LCIRC_SPEED);
       Right_Motor_ON_FORWARD(RIGHT_LCIRC_SPEED);
-      delay_time = CIR_SEC;                   // send delay time to global accessible by timer A1
-      TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
-      switch(--edges_left_to_drive)
-      {
-      case false:
-        instruction_label++; 
-        break;
-      default:
-        instruction_label = INSTRUCTION2; break;
-      } break;
-    default:      // turn off wheels and return permanently to main
+      delay_time = CIR_SEC;                         // send delay time to global accessible by timer A1
+      TA1CCTL2 |= CCIE;                             // enable timer A1.2 to count time
+      if(--circles_left_to_drive)
+        instruction_label = INSTRUCTION2; break     // drive in circles until there are no circles left to drive
+      else 
+        instruction_label++; break;                 // end shape routine
+      
+        
+    default:           /* turn off wheels and return permanently to main */
       Wheels_OFF();
       instruction_label = INSTRUCTION1;
-      edges_left_to_drive = NUM_TRIALS;
+      circles_left_to_drive = NUM_TRIALS;
     }
-  }
 }
+
+//**********
+// Figure-8*
+//**********
 void drive_in_figure8(void){
-  static uint8_t edges_left_to_drive = NUM_TRIALS;
+  static uint8_t figure8s_left_to_drive = NUM_TRIALS;
   static uint8_t instruction_label;
-  switch(shape_routine_begin)
-  {
-  case true:
+  if(shape_routine_begin)
     switch(instruction_label)
     {
-    case INSTRUCTION1: // wait two seconds before beginning
-      delay_time = TWO_SEC;                   // send delay time to global accessible by timer A1
-      TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
+        
+    case INSTRUCTION1: /* wait two seconds before beginning */
+      delay_time = TWO_SEC;                         // send delay time to global accessible by timer A1
+      TA1CCTL2 |= CCIE;                             // enable timer A1.2 to count time
       instruction_label++; break;
-    case INSTRUCTION2: // drive in circle counterclockwise
+        
+        
+    case INSTRUCTION2: /* drive in circle counterclockwise */
       Left_Motor_ON_FORWARD(LEFT_FIG8L_SPEED);
       Right_Motor_ON_FORWARD(RIGHT_FIG8L_SPEED);
-      delay_time = FOR_SEC;                   // send delay time to global accessible by timer A1
-      TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
+      delay_time = FOR_SEC;                        // send delay time to global accessible by timer A1
+      TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
       instruction_label++; break;
-    case INSTRUCTION3: // drive in circle clockwise
+        
+        
+    case INSTRUCTION3: /* drive in circle clockwise */
       Left_Motor_ON_FORWARD(LEFT_FIG8R_SPEED);
       Right_Motor_ON_FORWARD(RIGHT_FIG8R_SPEED);
-      delay_time = FOR_SEC;                   // send delay time to global accessible by timer A1
-      TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
-      switch(--edges_left_to_drive)
-      {
-      case false:
-        instruction_label++; 
-        break;
-      default:
-        instruction_label = INSTRUCTION2; break;
-      } break;
-    default:      // turn off wheels and return permanently to main
+      delay_time = FOR_SEC;                        // send delay time to global accessible by timer A1
+      TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
+      if(--figure8s_left_to_drive)
+        instruction_label = INSTRUCTION2; break    // drive in figure-8s until there are no more left to drive
+      else 
+        instruction_label++; break;                // end shape routine
+        
+        
+    default:          /* turn off wheels and return permanently to main */
       Wheels_OFF();
       instruction_label = INSTRUCTION1;
       edges_left_to_drive = NUM_TRIALS;
     }
-  }
 }
+
+//**********
+// Triangle*
+//**********
 void drive_in_triangle(void){
   static uint8_t edges_left_to_drive = NUM_TRIANGLE_EDGES;
   static uint8_t instruction_label;
-  switch(shape_routine_begin)
-  {
-  case true:
+  if(shape_routine_begin)
     switch(instruction_label)
     {
-    case INSTRUCTION1: // wait two seconds before beginning
-      delay_time = TWO_SEC;                   // send delay time to global accessible by timer A1
-      TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
+    case INSTRUCTION1: /* wait two seconds before beginning */
+      delay_time = TWO_SEC;                        // send delay time to global accessible by timer A1
+      TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
       instruction_label++;
       break;
-    case INSTRUCTION2: // drive along triangle edge
+        
+        
+    case INSTRUCTION2: /* drive along triangle edge */
       Left_Motor_ON_FORWARD(LEFT_FORWARD_SPEED);
       Right_Motor_ON_FORWARD(RIGHT_FORWARD_SPEED);
-      delay_time = ONE_SEC;                   // send delay time to global accessible by timer A1
-      TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
+      delay_time = ONE_SEC;                        // send delay time to global accessible by timer A1
+      TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
       instruction_label++; break;
-    case INSTRUCTION3: // turn 60 degrees counterclockwise
+        
+        
+    case INSTRUCTION3: /* turn 60 degrees counterclockwise */
       Left_Motor_ON_FORWARD(LEFT_LTURN_SPEED);
       Right_Motor_ON_FORWARD(RIGHT_LTURN_SPEED);
-      delay_time = ONE_SEC;                   // send delay time to global accessible by timer A1
-      TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
-      switch(--edges_left_to_drive)
-      {
-      case false:
-        instruction_label++; 
-        break;
-      default:
-        TA1CCTL2 |= CCIE;                    // enable timer A1.2 to count time
-        instruction_label = INSTRUCTION2; break;
-      } break;
-    default:      // turn off wheels and return permanently to main
+      delay_time = ONE_SEC;                        // send delay time to global accessible by timer A1
+      TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
+      if(--figure8s_left_to_drive)
+        instruction_label = INSTRUCTION2; break    // drive in figure-8s until there are no more left to drive
+      else 
+        instruction_label++; break;                 // end shape routine
+      
+      
+    default:          /* turn off wheels and return permanently to main */
       Wheels_OFF();
       instruction_label = INSTRUCTION1;
       edges_left_to_drive = NUM_TRIANGLE_EDGES;
     } break;
-  }
 }
