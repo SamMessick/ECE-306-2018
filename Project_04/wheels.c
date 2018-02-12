a923cb1bc7d8a7d1cf095ca3e94d49ca474414f2
shmessic <shmessic@ncsu.edu>
2018-02-12 12:43:25 -0500

    Update wheels.c

--- Project_04/wheels.c
+++ Project_04/wheels.c
@@ -62,8 +62,8 @@ void drive_in_circle(void){
         
         
     case INSTRUCTION2: /* drive in circle counterclockwise */
-      Left_Motor_ON_FORWARD(LEFT_LCIRC_SPEED);
-      Right_Motor_ON_FORWARD(RIGHT_LCIRC_SPEED);
+      L_FORWARD_on_time = LEFT_LCIRC_SPEED;
+      R_FORWARD_on_time = RIGHT_LCIRC_SPEED;
       delay_time = CIR_SEC;                         // send delay time to global accessible by timer A1
       TA1CCTL2 |= CCIE;                             // enable timer A1.2 to count time
       if(--circles_left_to_drive)
@@ -96,16 +96,16 @@ void drive_in_figure8(void){
         
         
     case INSTRUCTION2: /* drive in circle counterclockwise */
-      Left_Motor_ON_FORWARD(LEFT_FIG8L_SPEED);
-      Right_Motor_ON_FORWARD(RIGHT_FIG8L_SPEED);
+      L_FORWARD_on_time = LEFT_FIG8L_SPEED;
+      L_FORWARD_on_time = RIGHT_FIG8L_SPEED;
       delay_time = FOR_SEC;                        // send delay time to global accessible by timer A1
       TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
       instruction_label++; break;
         
         
     case INSTRUCTION3: /* drive in circle clockwise */
-      Left_Motor_ON_FORWARD(LEFT_FIG8R_SPEED);
-      Right_Motor_ON_FORWARD(RIGHT_FIG8R_SPEED);
+      L_FORWARD_on_time = LEFT_FIG8R_SPEED;
+      R_FORWARD_on_time = RIGHT_FIG8R_SPEED;
       delay_time = FOR_SEC;                        // send delay time to global accessible by timer A1
       TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
       if(--figure8s_left_to_drive)
