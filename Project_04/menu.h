87857795d3e26e5b1ac6a3559466b2c8d713471d
shmessic <shmessic@ncsu.edu>
2018-02-12 12:55:30 -0500

    Update menu.h

--- Project_04/menu.h
+++ Project_04/menu.h
@@ -65,3 +65,4 @@ extern volatile unsigned int update_display_count;
 extern volatile unsigned char display_changed;
 extern int8_t menu_counter;            // displays which option has been chosen by the buttons
 extern uint16_t delay_time;
+extern volatile unsigned int Time_Sequence;
