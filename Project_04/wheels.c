48e6e861a3b40d52557de3c053531539833a3184
shmessic <shmessic@ncsu.edu>
2018-02-12 12:37:29 -0500

    Update wheels.c

--- Project_04/wheels.c
+++ Project_04/wheels.c
@@ -13,95 +13,32 @@
 // H-bridge protection flags: 
 // don't allow for motion forward/backward simultaneously in one motor
 
-uint8_t left_forward_flag;
-uint8_t left_reverse_flag;
-uint8_t right_forward_flag;
-uint8_t right_reverse_flag;
-
 uint16_t delay_time;
-unsigned int Motors_Off_Time;         // Time out of 100ms that motors turn off
+unsigned int L_FORWARD_on_time;       // Time out of 100ms that left motor turns on
+unsigned int R_FORWARD_on_time;       // Time out of 100ms that right motor turns on 
 unsigned int Motors_Enabled;          // Flag set if motors are to run
 
 void WHEELS_test(void);
 
 void update_motor_state(void){
+    // Update motor's on/off state based on their PWM on/off times and Time_Sequence
     if(Motors_Enabled)
     {
-      if(Time_Sequence < Motors_Off_Time)
-        P3OUT |= (L_FORWARD | R_FORWARD);
-      
-      else if(Time_Sequence >= Motors_Off_Time)    
-        P3OUT &= ~(L_FORWARD | R_FORWARD);
+      if((Time_Sequence % PWM_PERIOD) < R_FORWARD_on_time)
+        P3OUT |=  R_FORWARD;
+      else if((Time_Sequence % PWM_PERIOD) >= R_FORWARD_on_time)    
+        P3OUT &= ~R_FORWARD;
+        
+      if((Time_Sequence % PWM_PERIOD) < L_FORWARD_on_time)
+        P3OUT |=  L_FORWARD;
+      else if((Time_Sequence % PWM_PERIOD) >= L_FORWARD_on_time)
+        P3OUT &= ~L_FORWARD;
     }
 }
 
-//------------------------
-// Turning wheels off
-//------------------------
-void Left_Motor_OFF(void){
-  TB0CCR3 = MOTOR_OFF;         // Set PWM to 0 percent duty cycle
-  TB0CCR4 = MOTOR_OFF;   
-  left_forward_flag = left_reverse_flag = false;
-}
-void Right_Motor_OFF(void){
-  TB0CCR5 = MOTOR_OFF;         // Set PWM to 0 percent duty cycle
-  TB0CCR6 = MOTOR_OFF;         
-  right_forward_flag = right_reverse_flag = false;
-}
 void Wheels_OFF(void){
-  Left_Motor_OFF();
-  Right_Motor_OFF();
-}
-
-//------------------------
-// Turning wheels on
-//------------------------
-//**********
-// Forward *
-//**********
-void Left_Motor_ON_FORWARD(uint8_t speed){
-  if(left_reverse_flag)                         // If left motor is not set to move backward already
-  {
-    TB0CCR4 = speed*DUTY_CYCLE_DIVISOR;         // Set left motor forward speed as one of 255 settings
-    left_forward_flag = true;                   // Prevent left motor from moving backward simultaneously
-  }
-}
-
-void Right_Motor_ON_FORWARD(uint8_t speed){
-  if(right_reverse_flag)                        // If right motor is not set to move backward already
-  { 
-    TB0CCR6 = speed*DUTY_CYCLE_DIVISOR;         // Set right motor forward speed as one of 255 settings
-    right_forward_flag = true;                  // Prevent right motor from moving backward simultaneously
-  }
-}
-
-void Drive_Straight_Forward(uint8_t speed){
-  Left_Motor_ON_FORWARD(speed);
-  Right_Motor_ON_FORWARD(speed);
-}
-  
-//**********
-// Reverse *
-//**********
-void Left_Motor_ON_REVERSE(uint8_t speed){
-  if(!left_forward_flag)                        // If left motor is not set to move forward already
-  { 
-    TB0CCR3 = speed*DUTY_CYCLE_DIVISOR;         // Set left motor reverse speed as one of 255 settings
-    left_reverse_flag = true;                   // Prevent left motor from moving forward simultaneously
-  }
-}
-
-void Right_Motor_ON_REVERSE(char speed){
-  if(!right_forward_flag)                       // If right motor is not set to move forward already
-  {
-    TB0CCR5 = speed*DUTY_CYCLE_DIVISOR;         // Set right motor reverse speed as one of 255 settings
-    right_reverse_flag = true;                  // Prevent right motor from moving forward simultaneously
-  }
-}
-
-void Drive_Straight_REVERSE(uint8_t speed){
-  Left_Motor_ON_REVERSE(speed);
-  Right_Motor_ON_REVERSE(speed);
+    L_FORWARD_on_time = Motors_OFF;
+    L_FORWARD_on_time = Motors_OFF;
 }
 
 //------------------------
@@ -201,16 +138,17 @@ void drive_in_triangle(void){
         
         
     case INSTRUCTION2: /* drive along triangle edge */
-      Left_Motor_ON_FORWARD(LEFT_FORWARD_SPEED);
-      Right_Motor_ON_FORWARD(RIGHT_FORWARD_SPEED);
+      Motors_Enabled = true;
+      L_FORWARD_on_time = LEFT_FORWARD_SPEED;
+      R_FORWARD_on_time = RIGHT_FORWARD_SPEED;
       delay_time = ONE_SEC;                        // send delay time to global accessible by timer A1
       TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
       instruction_label++; break;
         
         
     case INSTRUCTION3: /* turn 60 degrees counterclockwise */
-      Left_Motor_ON_FORWARD(LEFT_LTURN_SPEED);
-      Right_Motor_ON_FORWARD(RIGHT_LTURN_SPEED);
+      L_FORWARD_on_time = LEFT_LTURN_SPEED;
+      R_FORWARD_on_time = RIGHT_LTURN_SPEED;
       delay_time = ONE_SEC;                        // send delay time to global accessible by timer A1
       TA1CCTL2 |= CCIE;                            // enable timer A1.2 to count time
       if(--edges_left_to_drive)
